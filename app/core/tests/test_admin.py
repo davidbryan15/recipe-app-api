5fc47ed29dcc9e0cd43b783cabbb83c006fef950
davidbryan <aviles.davidb@gmail.com>
2023-11-27 11:38:15 -0500

    pushing code to check against codechecker

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,5 @@
 """
-Test for Django admin modifications.
+Tests for the Django admin modifications.
 """
 
 from django.test import TestCase
@@ -10,7 +10,7 @@ from django.test import Client
 class AdminSiteTest(TestCase):
   """Test for Django Admin"""
 
-  def setup(self):
+  def setUp(self):
     """Create user and client."""
     self.client = Client()
     self.admin_user = get_user_model().objects.create_superuser(
@@ -20,15 +20,22 @@ class AdminSiteTest(TestCase):
 
     self.client.force_login(self.admin_user)
     self.user = get_user_model().objects.create_user(
-      email="user@example/com",
+      email="user@example.com",
       password="testpass123",
       name="Test User"
     )
 
   def test_users_list(self):
-    """Test that users are listed on page/"""
-    url =  reverse("admin:core_user_changelist")
+    """Test that users are listed on page."""
+    url = reverse("admin:core_user_changelist")
     res = self.client.get(url)
 
     self.assertContains(res, self.user.name)
     self.assertContains(res, self.user.email)
+
+  def test_edit_user_page(self):
+    """Test the edit user page works"""
+    url = reverse("admin:core_user_change", args=[self.user.id])
+    res = self.client.get(url)
+
+    self.assertEqual(res.status_code, 200)
