0970eaaaae8efaeb4d5621109f79aea95dac6f68
davidbryan <aviles.davidb@gmail.com>
2023-11-27 11:20:16 -0500

    updated admin py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,17 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization.
+"""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+  """Define the admin pages for users."""
+  ordering = ["id"]
+  list_display = ["email", "name"]
 
 
-# Register your models here.
+
+admin.site.register(models.User, UserAdmin)
