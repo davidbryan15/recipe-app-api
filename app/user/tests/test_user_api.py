2a853ddc1cca05d711b3fedebc8241f3ec298638
davidbryan <aviles.davidb@gmail.com>
2024-06-25 23:18:48 -0400

    fixed error

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -111,7 +111,7 @@ class PublicUserApiTests(TestCase):
 
 
 class PrivateUserApiTests(TestCase):
-      """Test API requests that require authentication"""
+   """Test API requests that require authentication"""
 
    def setUp(self):
       self.user = create_user(
