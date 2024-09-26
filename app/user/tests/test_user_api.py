d63c81704b5c28d429a02d05c6ec2ea65a8638c7
davidbryan <aviles.davidb@gmail.com>
2024-09-26 15:50:02 -0400

    fix indent

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -66,19 +66,21 @@ class PublicUserApiTests(TestCase):
                         email=payload["email"]).exists()
       self.assertFalse(user_exists)
 
-def test_create_token_for_user(self):
+  def test_create_token_for_user(self):
       """Test Generates token for valid credentials."""
       user_details = {
-      'name': 'Test Name',
-      'email': 'test@example.com',
-      'password': 'test-user-password123'
-   }
+        'name': 'Test Name',
+        'email': 'test@example.com',
+        'password': 'test-user-password123'
+     }
       create_user(**user_details)
+
       payload = {
          'email': user_details['email'],
          'password': user_details['password']
       }
       res = self.client.post(TOKEN_URL, payload)
+
       self.assertIn('token', res.data)
       self.assertEqual(res.status_code, status.HTTP_200_OK)
 
