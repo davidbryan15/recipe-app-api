fe9ff7433f753f587814ef804f40b4847bb19735
davidbryan <aviles.davidb@gmail.com>
2024-10-08 16:43:30 -0400

    commiting to check work

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,11 @@
 """
 Views for the Recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -30,3 +30,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
   def perform_create(self, serializer):
     """create a new recipe"""
     serializer.save(user=self.request.user)
+
+class TagViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+  """Manage tags in db"""
+
+  serializer_class = serializers.TagSerializer
+  queryset = Tag.objects.all()
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticated]
+
+  def get_queryset(self):
+    return self.queryset.filter(user=self.request.user).order_by('-name')
